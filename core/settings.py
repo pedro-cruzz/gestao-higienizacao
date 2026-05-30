13ab5401795a853aa1cbe16b08b1f3fc466d3116
higorolv21 <profissionalolv@gmail.com>
2026-05-30 12:50:50 -0300

    Ajustei o sidebar, pois não estava aparecendo todos os botões

--- core/settings.py
+++ core/settings.py
@@ -66,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'service.context_processors.auth_roles',
             ],
         },
     },
@@ -120,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'inicio'
+LOGOUT_REDIRECT_URL = 'login'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
