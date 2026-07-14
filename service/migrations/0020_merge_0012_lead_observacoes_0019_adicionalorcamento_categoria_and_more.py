99294a09bf7b45212161e25580bbf036a821a857
pedro-cruzz <pedrocruzvb123@gmail.com>
2026-07-13 22:52:05 -0300

    fix: corrigindo erro de migração

---
+++ service/migrations/0020_merge_0012_lead_observacoes_0019_adicionalorcamento_categoria_and_more.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("service", "0012_lead_observacoes"),
+        ("service", "0019_adicionalorcamento_categoria_and_more"),
+    ]
+
+    operations = []
